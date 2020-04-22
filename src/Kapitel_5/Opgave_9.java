fabf7971f89f3d9f53a2ff85e5bca22e6de3eac2
Matiaslys <mati1857@edu.easj.dk>
2020-04-22 22:38:16 +0200

    så er hele kapitel 5 færdig

--- src/Kapitel_5/Opgave_9.java
+++ src/Kapitel_5/Opgave_9.java
@@ -7,38 +7,40 @@ public class Opgave_9 {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<>();
         ArrayList<Integer> scores = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
+        Scanner number = new Scanner(System.in);
+        Scanner Name = new Scanner(System.in);
         System.out.println("How many students do you have?");
-        int students = scanner.nextInt();
+        int students = number.nextInt();
         System.out.println("You have " + students + " students");
         for (int i = 1; i < students + 1; i++) {
             System.out.println(i + "/" + students);
             System.out.print("Name:");
-            // Maybe you dont understand why i use 2 String scanners. It is because when i made this exercise it wouldn't accept a user input if i only wrote 1 scanner.
-            // So i wrote the one i dont use first and the one i use second.
-//            String yes = scanner.nextLine();
-            String name = scanner.nextLine();
+            String name = Name.nextLine();
             names.add(name);
             System.out.print("Score:");
-            int score = scanner.nextInt();
+            int score = number.nextInt();
             scores.add(score);
         }
+
         //Sort
         if (scores.size() > 1) {
             for (int f = 0; f < scores.size() - 1; f++) {
-                for (int i = 1; i < scores.size() - i ; i++) {
+                for (int i = 1; i < scores.size(); i++) {
                     if (scores.get(i - 1) > scores.get(i)) {
                         int temp = scores.get(i - 1);
-                        System.out.println(i);
                         scores.set(i - 1, scores.get(i));
                         scores.set(i, temp);
+                        String backup = names.get(i - 1);
+                        names.set(i - 1, names.get(i));
+                        names.set(i, backup);
                     }
                 }
             }
         }
+
         System.out.println("Your students, sorted from lowest to highest is:");
         for (int i = 0; i < students; i++) {
-            System.out.println("| Name:" + names.get(i) +"  Score:"+ scores.get(i) + " |");
+            System.out.println("| Name:"+ names.get(i)+"  Score:"+ scores.get(i) + " |");
         }
     }
 }
